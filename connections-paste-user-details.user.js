3ff9680b312b8b3b2e25edfbd9b23b0088dee90c
pd-octo <paul.davidson@octoenergy.com>
2026-09-24 11:02:21 +0100

    Update connections-paste-user-details.user.js

--- connections-paste-user-details.user.js
+++ connections-paste-user-details.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kraken: Shell personal details
 // @namespace    https://github.com/pd-octo/tampermoney
-// @version      1.2.0
+// @version      1.2.1
 // @description  Paste single-line personal details from clipboard and auto-fill User details fields on Kraken
 // @author       Paul Davidson
 // @match        https://*.octopus.energy/*
